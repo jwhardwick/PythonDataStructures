d360175037eb4c5b9d3d3576c753f42f81ffd9f4
James Hardwick <jwhardwick@gmail.com>
2018-02-01 17:03:12 +1100

    Adds section for Binary.

---
+++ Bits/notes.py
@@ -0,0 +1,12 @@
+# Int to Binary
+num = 12
+binaryStr = "{0:b}".format(num)
+
+# Binary to Int
+toInt = int(binaryStr, 2)
+
+# OOP
+class ClassName:
+    def __init__(self):
+
+newClassObject = ClassName()
